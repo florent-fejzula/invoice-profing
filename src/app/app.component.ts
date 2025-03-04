930a50f68b465c24c759a6432c61744f67e102c5
Florent <dodi-messi19@hotmail.com>
2025-03-04 13:04:17 +0100

    Added commented code for adding a company programatically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth, onAuthStateChanged } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
 import { Router, NavigationEnd } from '@angular/router';
 
 // Import from firebase/auth
 import { setPersistence, browserLocalPersistence } from 'firebase/auth';
+// import { addDoc, collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +16,11 @@ export class AppComponent implements OnInit {
   user: any = null;
   showSidebar: boolean = false;
 
-  constructor(private auth: Auth, private router: Router) {}
+  constructor(private auth: Auth, private router: Router, private firestore: Firestore) {}
 
   ngOnInit() {
+    // this.addCompany();
+
     // ✅ Ensure Firebase Auth persists the session correctly
     setPersistence(this.auth, browserLocalPersistence)
       .then(() => {
@@ -37,6 +41,33 @@ export class AppComponent implements OnInit {
       }
     });
   }
+  
+  // async addCompany() {
+  //   const companiesCollection = collection(this.firestore, 'companies');
+
+  //   const companyData = {
+  //     EDB: "MK4038010502022",
+  //     accountNo: "300000003033323",
+  //     address: "ЃОЦЕ СТОЈЧЕВСКИ 3/3-12 СКОПЈЕ - БУТЕЛ",
+  //     bank: "КОМЕРЦИЈАЛНА БАНКА",
+  //     email: "intel.ofis@hotmail.com",
+  //     name: "ИНТЕЛ ОФИС ДООЕЛ",
+  //     ownerName: "Хабил Фејзула",
+  //     phone: "070302375",
+  //     settings: {
+  //       logo: true,
+  //       showTaxCategories: true
+  //     },
+  //     status: "active"
+  //   };
+
+  //   try {
+  //     await addDoc(companiesCollection, companyData);
+  //     console.log("Company added successfully!");
+  //   } catch (error) {
+  //     console.error("Error adding company:", error);
+  //   }
+  // }
 
   updateSidebarVisibility() {
     // ✅ Hide sidebar on login page, show it everywhere else if logged in
